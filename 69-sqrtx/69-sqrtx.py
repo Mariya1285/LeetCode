7ec335c59bb8f456348dee6a93d3d75bef2f5012
Mariya Johar <mariya1285@gmail.com>
2022-01-25 18:00:13 -0800

    Time: 63 ms (35.18%), Space: 14.1 MB (69.67%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.py
@@ -0,0 +1,21 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        # main = list(range(1,x))
+        start = 0
+        end = x
+        mid = start+end//2
+        
+        ans = -1
+        while start<=end:
+            sq = mid*mid
+            if sq==x:
+                return mid
+            if sq<x:
+                start = mid+1
+            else:
+                end = mid-1
+            
+            mid=(start+end)//2
+            
+        return end
+            
