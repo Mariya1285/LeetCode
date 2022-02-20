6fb483b4c39a11ee9cf96345ccae05f4c5d0e9c7
Mariya Johar <mariya1285@gmail.com>
2022-02-20 18:41:07 -0500

    Time: 56 ms (37.50%), Space: 13.9 MB (85.04%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,8 +1,24 @@
 class Solution:
     def fib(self, n: int) -> int:
+#         if n==0:
+#             return 0
+#         if n==1:
+#             return 1
+        
+#         return Solution.fib(self,n-1)+Solution.fib(self,n-2)
+
         if n==0:
             return 0
-        if n==1:
+        elif n==1:
             return 1
-        
-        return Solution.fib(self,n-1)+Solution.fib(self,n-2)
+        else:
+            pre = 0
+            post = 1
+            count = 2
+            while count<=n:
+                
+                temp=post
+                post=pre+post
+                pre=temp
+                count+=1
+            return post
