7639754fa1a53c963b1dc99c23688e5c62910ac1
Mariya Johar <mariya1285@gmail.com>
2022-06-07 01:17:43 -0400

    Time: 55 ms (32.85%), Space: 14 MB (23.64%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -3,7 +3,6 @@ class Solution:
         max_len = max(len(a),len(b))
         summ = list()
         carry = 0
-        print(max_len)
         a = a[::-1]
         b = b[::-1]
 
@@ -20,16 +19,11 @@ class Solution:
             except:
                 temp_b = 0
                 
-            print("a: ", temp_a)
-            print("b: ", temp_b)
             if temp_a ==1 and temp_b ==1:
-                print("11 case")
-                # if i!=max_len-1:
                 summ.append(0 + carry)
                 carry = 1
                 
             elif (temp_a == 0 or temp_b == 0) and (temp_a == 1 or temp_b == 1):
-                
                 if carry == 0:
                     summ.append(1)
                     carry = 0
@@ -42,16 +36,10 @@ class Solution:
                 else:
                     summ.append(0)
                 carry = 0
-            print("sum: ",summ)
             
         if max_len==1 and (a!="0" and b!="0"):
-            print("last carry append")
-            print(summ)
             summ.append(carry)
-            print(summ)
         elif max_len!=1 and carry ==1:
-            print(summ)
             summ.append(carry)
-            print(summ)
         return ("").join(str(x) for x in summ[::-1])
                 
