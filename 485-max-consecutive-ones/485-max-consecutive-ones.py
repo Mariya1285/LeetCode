914c6083b6ad5b23ddf8e4e33ce7fcad21dfe166
Mariya Johar <mariya1285@gmail.com>
2022-06-10 09:53:26 -0400

    Time: 532 ms (30.01%), Space: 14.3 MB (31.23%) - LeetHub

--- 485-max-consecutive-ones/485-max-consecutive-ones.py
+++ 485-max-consecutive-ones/485-max-consecutive-ones.py
@@ -1,72 +1,14 @@
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        start, end = 0, 1
+        count = 0
         max_len = 0
-        if len(nums)==1:
-            if nums[0]==0:
-                return 0
+        for i in range(len(nums)):
+            if nums[i]==1:
+                count+=1
             else:
-                return 1
-        else:
-            while end<len(nums):
-                print("end: ",end)
-                print("start: ", start)
-                if end!=len(nums)-1:
-                    if nums[end]==1:
-                        end+=1
-                    else:
-                        print("end: ",end)
-                        print("start: ", start)
-                        if (end-start)>max_len:
-                            max_len=end-start
-                            print("end element 0")
-                        else:
-                            pass
-
-                        end+=1
-                        start=end
-                        print("MAX LEn: ", max_len)
-                elif nums[end]==0 and end==len(nums)-1:
-                    print("I m here")
-                    if nums[end-1]==1:
-                        if start!=0:
-                            if (end-start+1)>max_len:
-                                max_len=end-start+1
-                            else:
-                                pass
-                        else:
-                            if (end-start)>max_len:
-                                max_len=end-start
-                            else:
-                                pass
-                    else:
-                        pass
-
-                    end+=1
-                    start=end
-                    print("end max_len: ", max_len)
-                
-                elif nums[end]==1 and end==len(nums)-1:
-                    if start!=0:
-                        if (end-start+1)>max_len:
-                                max_len=end-start+1
-                        else:
-                            pass
-                    else:
-                        if nums[end-1]!=1:
-                            if (end-start)>max_len:
-                                max_len=end-start 
-                                return max_len
-                            else:
-                                pass
-                        else:
-                            if (end-start+1)>max_len:
-                                max_len=end-start+1 
-                                return max_len
-                            else:
-                                pass
-                    end+=1
-                    start=end
-                
-            print("**********************************")
-            return max_len
+                if count>max_len:
+                    max_len = count
+                count = 0
+        if count>max_len:
+            max_len = count
+        return max_len
