1e4ee45d36df0c00c772df71bae1caf113b3f4aa
Mariya Johar <mariya1285@gmail.com>
2022-07-20 22:31:26 -0400

    Time: 122 ms (25.52%), Space: 14 MB (94.04%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,14 +1,17 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        flag = True
-        ransomNote_dict = dict(Counter(ransomNote))
-        magazine_dict = dict(Counter(magazine))
-        for letter, count_of_letter in ransomNote_dict.items():
-            if letter in magazine_dict:
-                if count_of_letter>magazine_dict[letter]:
-                    return False
-                else:
-                    pass
+        magazine_count = dict()
+        for letter in magazine:
+            if letter in magazine_count:
+                magazine_count[letter]+=1
             else:
+                magazine_count[letter] = 1
+        
+        for letter in ransomNote:
+            if letter not in magazine_count:
                 return False
+            elif magazine_count[letter] == 0:
+                return False
+            else:
+                magazine_count[letter]-=1
         return True
